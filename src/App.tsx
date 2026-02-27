954aefedcb38fd3b68367efcd4de755508512d9e
Md. Asaduzzman Arabin <aznarabin@gmail.com>
2026-02-27 15:23:30 +0600

    Initialized app with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+import './index.css';
+
+
+function App() {
+
+  return (
+    <div className="min-h-full flex items-center justify-center">
+        <Button>Alhamdulillah</Button>
+      </div>
+  )
+}
+
+export default App
